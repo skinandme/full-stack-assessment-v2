364155b3f203fca210017fecfa912ad5509acbc7
MC <matt.choplin@skinandme.com>
2023-04-20 20:01:10 +0100

    Fix wrong test

--- front-end/src/App.test.tsx
+++ front-end/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Buy Now button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Buy Now/i);
   expect(linkElement).toBeInTheDocument();
 });
