36a5ba5f92307c28e42841fbc844f7df9c1c1284
Mathew Trivett <mathew.trivett@skinandme.com>
2023-11-15 13:39:48 +0000

    feat: cleanup

--- front-end/src/api/foo.tsx
+++
@@ -1,7 +0,0 @@
-const foo = async () => {
-  const response = await fetch("http://localhost:9000/foo");
-
-  return await response.text();
-};
-
-export default foo;
--- front-end/src/pages/BillingPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BillingPage: React.FC = () => {
-  return <div>TODO - BILLING PAGE</div>;
-};
-
-export default BillingPage;
