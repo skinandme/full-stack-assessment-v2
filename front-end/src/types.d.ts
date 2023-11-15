957e8d6832a2415d22d80585bc094476cff68d42
Mathew Trivett <mathew.trivett@skinandme.com>
2023-11-15 13:41:30 +0000

    feat: define checkout interfaces

---
+++ front-end/src/types.d.ts
@@ -0,0 +1,19 @@
+export interface IProduct {
+	id: number;
+	sku: string;
+	name: string;
+	unit_price: number;
+}
+
+export interface ICheckoutItem {
+	id: number;
+	product: IProduct;
+	quantity: number;
+	sub_total: number;
+}
+
+export interface ICheckout {
+	id: number;
+	currency: "GBP";
+	items: ICheckoutItem[];
+}
